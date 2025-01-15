5f12c28e452d5eedf7906405674a7fcf63d1b4a8
John Lorenz Inocentes <geraldinedelima66@gmail.com>
2025-01-16 01:35:36 +0800

    Add user management

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Signup from "./pages/Signup";
 import { ToastContainer } from "react-toastify";
+import UserManagementPage from "./pages/UserManagement/UserManagement";
 const App = () => {
   return (
     <Router>
       <ToastContainer />
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/admin/user-management" element={<UserManagementPage />} />
       </Routes>
     </Router>
   );
