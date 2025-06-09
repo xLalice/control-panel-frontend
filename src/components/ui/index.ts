1a3a1519f0e99d61d936bfb104a0dc3f16806fd8
John Lorenz Inocentes <dreamsend08@gmail.com>
2025-06-09 21:11:43 +0800

    feat: add UI component exports to index file

---
+++ src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./select";
+export { Input } from "./input";
+export { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./table";
+export { Badge } from "./badge";
+export { Card, CardContent, CardHeader, CardTitle } from "./card";
+export { Button } from "./button";
+export { Skeleton } from "./skeleton";
