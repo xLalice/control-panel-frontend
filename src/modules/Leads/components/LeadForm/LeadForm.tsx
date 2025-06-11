e04a3928730f6dfd21b29a01ef90c148ac614003
John Lorenz Inocentes <geraldinedelima66@gmail.com>
2025-06-11 22:08:36 +0800

    feat: refactor LeadForm and LeadFormContent for improved state management and UI consistency

--- src/modules/Leads/components/LeadForm/LeadForm.tsx
+++ src/modules/Leads/components/LeadForm/LeadForm.tsx
@@ -2,54 +2,40 @@ import { Plus } from "lucide-react";
 import { LeadFormProps } from "../../types/leads.types";
 import { Dialog, DialogTrigger, Button } from "@/components/ui";
 import { LeadFormContent } from "./LeadFormContent";
-import { useState } from "react";
 import { toast } from "react-toastify";
 import { useLeadsTable } from "../../hooks/useLeadsTable";
 
-const LeadForm = ({ lead, users }: LeadFormProps) => {
+const LeadForm = ({ lead, users, isOpen, onClose }: LeadFormProps) => {
   const isEditMode = !!lead;
-  const [isOpen, setIsOpen] = useState(false);
   const { refetchLeads } = useLeadsTable();
-
-  const handleOpenChange = (newOpenState: boolean) => {
-    setIsOpen(newOpenState);
-  };
 
   const handleLeadFormSuccess = () => {
     toast.success(
       isEditMode ? "Lead updated successfully!" : "Lead created successfully!"
     );
-    setIsOpen(false);
     if (refetchLeads) {
       refetchLeads();
     }
+
+    onClose();
   };
 
-  const handleLeadFormClose = () => {
-    setIsOpen(false);
-  };
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      {!isEditMode && (
+        <DialogTrigger asChild>
+          <Button className="ml-auto bg-yellow-500 hover:scale-110 hover:bg-yellow-600 text-black h-8 text-xs">
+            <Plus className="w-3 h-3 mr-1" />
+            Create Lead
+          </Button>
+        </DialogTrigger>
+      )}
 
-  return isEditMode ? (
-    <Dialog open={isOpen} onOpenChange={handleOpenChange}>
       <LeadFormContent
         lead={lead}
         users={users}
         onSuccess={handleLeadFormSuccess}
-        onClose={handleLeadFormClose}
-      />
-    </Dialog>
-  ) : (
-    <Dialog open={isOpen} onOpenChange={handleOpenChange}>
-      <DialogTrigger asChild>
-        <Button className="ml-auto bg-yellow-500 hover:scale-110 hover:bg-yellow-600 text-black h-8 text-xs">
-          <Plus className="w-3 h-3 mr-1" />
-          Create Lead
-        </Button>
-      </DialogTrigger>
-      <LeadFormContent
-        users={users}
-        onSuccess={handleLeadFormSuccess}
-        onClose={handleLeadFormClose}
+        onClose={onClose}
       />
     </Dialog>
   );
