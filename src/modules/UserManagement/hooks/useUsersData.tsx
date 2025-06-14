85086a24befd40a159342df50691594b3c4a8886
John Lorenz Inocentes <dreamsend08@gmail.com>
2025-06-15 03:53:23 +0800

    feat: Refactor user fetching logic to use custom useUsersData hook

---
+++ src/modules/UserManagement/hooks/useUsersData.tsx
@@ -0,0 +1,11 @@
+import { fetchUsers } from "@/api/api";
+import { User } from "@/types";
+import { useQuery } from "@tanstack/react-query";
+
+export const useUsersData =  () => useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const response = await fetchUsers();
+      return response;
+    },
+  });
