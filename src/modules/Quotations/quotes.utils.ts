2c241e492d350c9a6f570c2c792173510a8e63f8
John Lorenz Inocentes <lorenzinocentes@gmail.com>
2026-01-23 03:13:00 +0800

    feat: add converted status

--- src/modules/Quotations/quotes.utils.ts
+++ src/modules/Quotations/quotes.utils.ts
@@ -5,6 +5,7 @@ export const getStatusColor = (status: string) => {
       case QuotationStatus.Draft : return "bg-gray-500 hover:bg-gray-600";
       case QuotationStatus.Sent : return "bg-blue-500 hover:bg-blue-600";
       case QuotationStatus.Accepted : return "bg-green-500 hover:bg-green-600";
+      case QuotationStatus.Converted: return "bg-emerald-500 hover:bg-emerald-600";
       case QuotationStatus.Rejected : return "bg-red-500 hover:bg-red-600";
       default: return "bg-gray-500";
     }
